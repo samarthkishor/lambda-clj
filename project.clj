8c601906356f702dd901053eef36917cf8deae74
Samarth Kishor <samarthkishor1@gmail.com>
2019-04-01 00:33:23 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject lambda "0.1.0-SNAPSHOT"
+  :description "An untyped Lambda Calculus implementation in Clojure"
+  :url "http://github.com/samarthkishor/lambda"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns lambda.core})
